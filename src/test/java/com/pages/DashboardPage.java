a3ff79f582c70ed811864543b316c8bae4396664
Mehmet Buyukertas <acizkul6327@gmail.com>
2021-04-28 17:53:25 +0200

    Create classes

---
+++ src/test/java/com/pages/DashboardPage.java
@@ -0,0 +1,13 @@
+package com.pages;
+
+import com.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+public class DashboardPage extends  BasePage {
+    public DashboardPage() {
+        PageFactory.initElements(Driver.get(), this);
+
+    }
+}
